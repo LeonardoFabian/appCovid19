7299a5c73a60b2b64461305cd301caa30364c27a
LeonardoFabian <eddy24kfabian@gmail.com>
2020-06-11 23:48:07 -0400

    eliminando codigo innecesario
    Elimininando codigo innecesario

--- covid19.sql
+++ covid19.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2020 a las 16:42:57
+-- Tiempo de generación: 12-06-2020 a las 05:24:09
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
 
@@ -34,6 +34,8 @@ CREATE TABLE `infected` (
   `lastname` text COLLATE utf8_spanish_ci DEFAULT NULL,
   `gender` text COLLATE utf8_spanish_ci DEFAULT NULL,
   `birthday` datetime DEFAULT NULL,
+  `signo_zodiacal` text COLLATE utf8_spanish_ci DEFAULT NULL,
+  `simbolo_zodiacal` text COLLATE utf8_spanish_ci DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
   `country` text COLLATE utf8_spanish_ci DEFAULT NULL,
   `nationality` text COLLATE utf8_spanish_ci DEFAULT NULL,
@@ -47,13 +49,6 @@ CREATE TABLE `infected` (
   `pic_thumbnail` text COLLATE utf8_spanish_ci DEFAULT NULL,
   `pic_large` text COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `infected`
---
-
-INSERT INTO `infected` (`id`, `firstname`, `lastname`, `gender`, `birthday`, `age`, `country`, `nationality`, `email`, `phone`, `cell`, `street`, `hnumber`, `latitude`, `longitude`, `pic_thumbnail`, `pic_large`) VALUES
-(1, 'Ramón', 'Fabián', 'male', '2014-10-08 00:00:00', 32, 'Dominican Republic', 'DO', 'ramonlfabian@gmail.com', '8095614038', '8293224973', 'C/Francisco Cardenas', 6, '69.942300', '108.034100', 'https://randomuser.me/api/portraits/thumb/women/68.jpg', 'https://randomuser.me/api/portraits/women/68.jpg');
 
 --
 -- Índices para tablas volcadas
@@ -73,7 +68,7 @@ ALTER TABLE `infected`
 -- AUTO_INCREMENT de la tabla `infected`
 --
 ALTER TABLE `infected`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
